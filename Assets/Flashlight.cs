ed8bf114dee8f279f47239956d66937de08e8b19
Tony Godfrey <tonygod@sharkappsllc.com>
2017-03-09 13:32:55 -0800

    initial commit

---
+++ Assets/Flashlight.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour {
+    public Light lamp;
+
+    bool switchedOn = false;
+    float _intensity;
+
+    private void Awake()
+    {
+        _intensity = lamp.intensity;
+        if (!switchedOn)
+            lamp.intensity = 0f;
+    }
+
+    public void ToggleSwitch()
+    {
+        switchedOn = !switchedOn;
+        if (switchedOn)
+        {
+            lamp.intensity = _intensity;
+        }
+        else
+        {
+            lamp.intensity = 0f;
+        }
+    }
+}
